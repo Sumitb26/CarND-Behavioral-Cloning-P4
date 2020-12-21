1acf045abf1f3cf4a977d4915754465f892794e7
root <bhirud.sumit@gmail.com>
2020-12-21 17:38:14 +0000

    model restructure

--- model.py
+++ model.py
@@ -3,45 +3,41 @@ import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
-lines = []
+from PIL import Image
+
+car_images = []
+steering_angles = []
 with open('train_data_map1/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
-        
-images = []
-measurements = []
-lines.pop(0)
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        print(source_path)
-        filename = source_path.split('/')[-1]
-        current_path = 'train_data_map1/IMG/' + filename
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
-    
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+    for row in reader:
+        steering_center = float(row[3])
+        correction = 0.2 # this is a parameter to tune
+        steering_left = steering_center + correction
+        steering_right = steering_center - correction
+
+        # read in images from center, left and right cameras
+        img_center = cv2.imread(row[0])
+        img_left = cv2.imread(row[1])
+        img_right = cv2.imread(row[2])
+        # add images and angles to data set
+        car_images.extend((img_center, img_left, img_right, np.fliplr(img_center), np.fliplr(img_left), np.fliplr(img_right)))
+        steering_angles.extend((steering_center, steering_left, steering_right, -steering_center, -steering_left, -steering_right))
+
+X_train = np.array(car_images)
+y_train = np.array(steering_angles)
+print(len(X_train))
+print(len(y_train))
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(25,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(64,3,3, activation="relu"))
-model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Conv2D(25,(5,5),strides=(2,2), activation="relu"))
+model.add(Conv2D(36,(5,5),strides=(2,2), activation="relu"))
+model.add(Conv2D(48,(5,5),strides=(2,2), activation="relu"))
+model.add(Conv2D(64,(3,3), activation="relu"))
+model.add(Conv2D(64,(3,3), activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
